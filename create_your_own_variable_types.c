c7c3c2a155eda21ca9deaef14412d59f3cf55c9b
WizzOFF <maher_off@proton.me>
2025-03-14 18:51:31 +0100

    create_your_own_variable_types: use a structure in a function

--- create_your_own_variable_types.c
+++ create_your_own_variable_types.c
@@ -5,7 +5,7 @@ struct Coordonnees
     int y; // Ordonnées
 };
 */
-
+/*
 struct Personne
 {
     char nom[100];
@@ -15,3 +15,13 @@ struct Personne
     int age;
     int etudiant; // Booléen : 1 = etudiant, 0 = non etudiant
 };
+*/
+
+#include "main.h" // Inclusion du .h qui contient les prototypes et structures
+
+int main(int argc, char *argv[])
+{
+    struct Coordonnees point; // Création d'une variable "point" de type Coordonnees
+
+    return 0;
+}
