83a71aff6dd2feb8f3fe93e7548571a7ac879ef5
WizzOFF <maher_off@proton.me>
2025-03-14 18:45:55 +0100

    create_your_own_variable_types: tables in a structure

--- create_your_own_variable_types.c
+++ create_your_own_variable_types.c
@@ -1,6 +1,17 @@
-
+/*
 struct Coordonnees
 {
     int x; // Abscisses
     int y; // Ordonnées
+};
+*/
+
+struct Personne
+{
+    char nom[100];
+    char prenom[100];
+    char adresse[1000];
+    
+    int age;
+    int etudiant; // Booléen : 1 = etudiant, 0 = non etudiant
 };
